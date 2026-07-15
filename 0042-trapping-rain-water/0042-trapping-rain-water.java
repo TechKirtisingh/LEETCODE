d1f27885f1fd420dbf6669332423a9d9f2f215cc
Kirti Singh <kirtisingh77852@gmail.com>
2026-07-16 00:08:10 +0530

    Time: 1 ms (60.6%), Space: 48.8 MB (16.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,30 +1,37 @@
 class Solution {
     public int trap(int[] height) 
     {
-        int n = height.length;
+      //  int n = height.length; // store length
 
-        int [] leftMax = new int[n];
+// to know how much water store we need to calculate 
+
+        int [] leftMax = new int[height.length]; //left height of the wall
 
         leftMax[0] = height[0];
 
-        for(int i = 1 ; i<n ; i++)
+        for(int i = 1 ; i<height.length ; i++)
         {
-            leftMax[i] = Math.max(leftMax[i-1] , height[i]);
+            leftMax[i] = Math.max(leftMax[i-1] , height[i]); // comparing height of wall and leftmax[0] start with 0 
         }
-        int[] rightMax = new int[n];
+        int[] rightMax = new int[height.length];
 
-        rightMax[n-1] = height[n-1];
+        rightMax[height.length-1] = height[height.length-1];
 
-        for(int i=n-2 ; i>=0 ; i--)
+        for(int i=height.length-2 ; i>=0 ; i--)
         {
             rightMax[i] = Math.max(rightMax[i+1] , height[i]);
         }
 
         int ans =0;
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<height.length;i++)
         {
-            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+
+// after calculating maximum from both left and right max
+// then we find min of both because utna hi water store ho payega
+// and the we calculate the minheigth - the actual heigth of wall so that to calculate how much water is store
+
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i]; 
         }
 
         return ans;
