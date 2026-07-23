6edecfb9bce4a2839a89f5640f7b8f51c9062693
Kirti Singh <kirtisingh77852@gmail.com>
2026-07-24 02:26:50 +0530

    Time: 2 ms (22.42%), Space: 43.3 MB (14.65%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,7 +1,10 @@
 class MyStack {
 
+// no one access the queue
     private Queue<Integer> main;
+
     private Queue<Integer> helper;
+
 
     public MyStack() 
     {
@@ -11,6 +14,10 @@ class MyStack {
            
     }
     
+    // main->helper
+    //main.add(ele)
+    //helper->main
+
     public void push(int x) 
     {
         // Moving all elemnt from main to helper
@@ -19,9 +26,16 @@ class MyStack {
         {
             helper.add(main.remove());
         }
+
+// see in notes::->>
+
+        // before adding element in main queue the element present in main queue will be added in helper 
+        
+        // then fir helper se elemnt nikal kr wapas main queue me add kr denge::
         // add ele(x) in main queue
 
         main.add(x);
+        
         // moving all element from helper to main   
 
         while(helper.size()>0)
@@ -30,16 +44,19 @@ class MyStack {
         }
     }
     
+
     public int pop() 
     {
         return main.remove();   
     }
     
+
     public int top() 
     {
         return main.peek();   
     }
     
+
     public boolean empty() 
     {
         if(main.size()==0)
