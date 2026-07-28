4287edf897671180d526cda58c4d0ebb4b0a2299
Kirti Singh <kirtisingh77852@gmail.com>
2026-07-29 01:38:49 +0530

    Time: 14 ms (29.7%), Space: 46.3 MB (30.4%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+// Sab unwanted characters remove karo.
+// replaceAll(regex, replacement)
+// regex → kya find karna hai.
+//replacement → uski jagah kya rakhna hai.
+
+//Jo bhi character letter ya digit nahi hai.[^(not)]
+
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+
+            if(s.charAt(left) != s.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
