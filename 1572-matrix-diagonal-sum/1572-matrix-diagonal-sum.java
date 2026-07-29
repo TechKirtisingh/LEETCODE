eeb5d2c1a4eaeeb5f85bbe2716f8924055bcd1a4
Kirti Singh <kirtisingh77852@gmail.com>
2026-07-29 19:14:19 +0530

    Time: 1 ms (35.4%), Space: 46.6 MB (14.13%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+
+        int sum = 0;
+        int n = mat.length;
+
+//i + j == n - 1 secondary diagonal ko identify karne ka formula hai.
+//n = row,column 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == j || i + j == n - 1) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int diagonalSum(int[][] mat) 
+//     {
+//         int sum =0;
+
+//         int n = mat.length; 
+
+//         for(int i=0 ; i<n ; i++)
+//         {
+//             sum +=mat[i][i]; // primary Diagonal
+
+//             sum +=mat[i][n-1-i]; //secondary diagonal
+//         }  
+
+//         // Remove Duplicate Center Elements
+
+//         if(n%2 ==1)
+//         {
+//             sum = sum - mat[n/2][n/2];
+//         }
+
+//         return sum;
+//     }
+// }
