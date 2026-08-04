34768b72320d7630ee872f8b6f92395bbe141d30
Kirti Singh <kirtisingh77852@gmail.com>
2026-08-04 20:02:16 +0530

    Time: 0 ms (100%), Space: 43.8 MB (53.81%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,26 +1,76 @@
-class Solution {
-    public void rotate(int[][] matrix) {
+// use of both TRANSPOSE AND REVERSE 
 
-        int n = matrix.length;
+class Solution{
+    public void rotate(int[][] matrix)
+    {
+        int n= matrix.length;
 
-        int[][] ans = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-
-            for (int j = 0; j < n; j++) {
-
-                //Original matrix ka element uthao aur uski rotated position par rakh do.
-
-                ans[j][n - 1 - i] = matrix[i][j];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1 ; j<n ;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
 
-        for (int i = 0; i < n; i++) {
+        // Reverse
 
-            for (int j = 0; j < n; j++) {
+        for(int i=0 ; i<n ; i++)
+        {
+            int left =0;
+            int right = n-1;
 
-                matrix[i][j] = ans[i][j];
+            while(left<right)
+            {
+                // Temporary variable mein left wali value store karte hain.              
+                int temp = matrix[i][left];
+                //Right wali value ko left mein rakh do.
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+// another method
+
+// class Solution {
+//     public void rotate(int[][] matrix) {
+
+//         int n = matrix.length;
+
+//         int[][] ans = new int[n][n];
+
+//         for (int i = 0; i < n; i++) {
+
+//             for (int j = 0; j < n; j++) {
+
+//                 //Original matrix ka element uthao aur uski rotated position par rakh do.
+
+//                 ans[j][n - 1 - i] = matrix[i][j];
+//             }
+//         }
+
+//         for (int i = 0; i < n; i++) {
+
+//             for (int j = 0; j < n; j++) {
+
+//                 matrix[i][j] = ans[i][j];
+//             }
+//         }
+//     }
+// }
