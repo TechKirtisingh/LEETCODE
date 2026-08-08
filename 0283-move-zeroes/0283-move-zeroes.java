ef94bab056836ed4b7ff2feeb7078d53c9a9fbfd
Kirti Singh <kirtisingh77852@gmail.com>
2026-08-08 18:16:23 +0530

    Time: 1 ms (99.89%), Space: 47.8 MB (38.81%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,24 +1,55 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-
-// i se array traverse karo
-// 2. Agar nums[i] != 0:
-//       nums[i] ko nums[j] se swap karo
-//       j++
-//3. End mein saare zero automatically aa jayenge
-
         int j = 0;
 
+        // Step 1: saare non-zero elements front mein
         for (int i = 0; i < nums.length; i++) {
 
             if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-
+                nums[j] = nums[i];
                 j++;
             }
         }
+
+        // Step 2: remaining positions mein zero
+        while (j < nums.length) {
+            nums[j] = 0;
+            j++;
+        }
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+
+
+// // i se array traverse karo
+// // 2. Agar nums[i] != 0:
+// //       nums[i] ko nums[j] se swap karo
+// //       j++
+// //3. End mein saare zero automatically aa jayenge
+
+//         int j = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+
+//             if (nums[i] != 0) {
+//                 int temp = nums[i];
+//                 nums[i] = nums[j];
+//                 nums[j] = temp;
+
+//                 j++;
+//             }
+//         }
+//     }
+// }
+
+
+
+
