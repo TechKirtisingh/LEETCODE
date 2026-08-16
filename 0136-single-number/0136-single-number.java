928c0e4e78d4d3f669033ccdc26eaaf2be7a1ee6
Kirti Singh <kirtisingh77852@gmail.com>
2026-08-16 17:22:20 +0530

    Time: 1 ms (99.94%), Space: 46.9 MB (65.68%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,19 +1,55 @@
-class Solution {
+class Solution{
 
-    public int singleNumber(int[] nums) {
+    public int singleNumber(int[] nums) 
+    {
+        // [1,2,1,4,4]
 
-        Arrays.sort(nums);
+        // variable to store xor element
+        int sing = nums[0]; //1
 
-        for(int i = 0; i < nums.length - 1; i += 2){
-
-            if(nums[i] != nums[i + 1]){
-                return nums[i];
-            }
+        //start a loop for all element
+        for(int i=1 ; i<nums.length ; i++)
+        {
+            sing = sing^nums[i]; // 1^2^1^4^4 // 1 xor 1 = 4 xor 4 = 0 = 0 xor 2 = 2
         }
 
-        return nums[nums.length - 1];
+        return sing;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// brute force technique....
+
+
+// class Solution {
+
+//     public int singleNumber(int[] nums) {
+
+//         Arrays.sort(nums);
+
+//         for(int i = 0; i < nums.length - 1; i += 2){
+
+//             if(nums[i] != nums[i + 1]){
+//                 return nums[i];
+//             }
+//         }
+
+//         return nums[nums.length - 1];
+//     }
+// }
+
+
+
+
 
 
 
