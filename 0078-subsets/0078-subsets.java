99124346eb975fd256e11556828c48a343623c8e
Kirti Singh <kirtisingh77852@gmail.com>
2026-09-13 12:18:37 +0530

    Time: 1 ms (87.18%), Space: 44.3 MB (38.55%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,65 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsets(int[] nums) 
+    {
 
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> resultList = new ArrayList<>();
 
-        int n = nums.length;
-        int total = 1 << n;   // 2^n
+        // Start backtracking from the beginning
 
-        for (int mask = 0; mask < total; mask++) {
+        backtrack(resultList , new ArrayList<>(), nums , 0);
+        return resultList;
 
-            List<Integer> subset = new ArrayList<>();
+    }
+    private void backtrack(List<List<Integer>> resultSets , List<Integer> tempSet, int[] nums , int start)
+    {
+        //Add the set to result set
 
-            for (int i = 0; i < n; i++) {
+        resultSets.add(new ArrayList<>(tempSet));
 
-                // Check if ith bit is set
-                if ((mask & (1 << i)) != 0) {
-                    subset.add(nums[i]);
-                }
-            }
+        for(int i= start ; i<nums.length;i++)
+        {
+            //Case of including the number
+            tempSet.add(nums[i]);
 
-            result.add(subset);
+            //Backtrack the new subset
+            backtrack(resultSets , tempSet , nums, i+1);
+
+            //Case of not-including the number
+            tempSet.remove(tempSet.size() -1);
         }
-
-        return result;
     }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+
+//         List<List<Integer>> result = new ArrayList<>();
+
+//         int n = nums.length;
+//         int total = 1 << n;   // 2^n
+
+//         for (int mask = 0; mask < total; mask++) {
+
+//             List<Integer> subset = new ArrayList<>();
+
+//             for (int i = 0; i < n; i++) {
+
+//                 // Check if ith bit is set
+//                 if ((mask & (1 << i)) != 0) {
+//                     subset.add(nums[i]);
+//                 }
+//             }
+
+//             result.add(subset);
+//         }
+
+//         return result;
+//     }
+// }
